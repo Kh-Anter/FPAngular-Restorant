ca2a32bcba1c2d0fdae283fbb35c6b94683fc878
Kh-Anter <105430867+Kh-Anter@users.noreply.github.com>
2022-11-04 12:56:59 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './body/categories/categories.component';
+import { AboutusComponent } from './body/aboutus/aboutus.component';
+import { ShopComponent } from './body/shop/shop.component';
+import { OfferComponent } from './body/offer/offer.component';
+import { BestDeliveryComponent } from './body/best-delivery/best-delivery.component';
+import { ReviewsComponent } from './body/reviews/reviews.component';
+import { AlloffersComponent } from './body/alloffers/alloffers.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoriesComponent,
+    AboutusComponent,
+    ShopComponent,
+    OfferComponent,
+    BestDeliveryComponent,
+    ReviewsComponent,
+    AlloffersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+    
+  ],
+  providers: [],
+   bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  constructor() {
+   }
+
+  ngOnInit(): void {
+    var brand=document.querySelector(".brand");
+    var nav =document.querySelector("nav");
+    var nav_btn=document.getElementById("nav-btn");
+    var navLink=document.querySelectorAll(".nav-link");
+    window.addEventListener("scroll",function(){
+      if(this.window.pageYOffset>100){
+        brand?.classList.remove("text-white");
+        brand?.classList.add("text-dark");
+        nav?.classList.add("bg-light")
+        nav?.classList.add("activeNav");
+        nav?.classList.remove("topNav");
+        nav_btn?.classList.add("text-dark");
+        navLink.forEach((e)=>{
+        e.classList.remove("text-white");
+        e.classList.add("text-dark");
+       });
+      }else{
+        brand?.classList.remove("text-dark");
+        brand?.classList.add("text-white");
+        nav?.classList.remove("bg-light")
+        nav?.classList.add("topNav")
+        nav?.classList.add("activeNav");
+        nav_btn?.classList.remove("text-dark");
+        navLink.forEach((e)=>{
+          e.classList.remove("text-dark");
+          e.classList.add("text-white");
+         });
+      }
+    });
+
+  }
+}
