f2ff6691c572553353090f0d6079ffe30e2620a3
Ahmed Khalaf <ahmedkhalaf22696@gmail.com>
2022-11-09 23:26:28 +0200

    ahmed

--- src/app/body/profile/favourite/favourite.component.ts
+++ src/app/body/profile/favourite/favourite.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FavouriteService} from '../../../services/favourite.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-favourite',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavouriteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private FavouriteService:FavouriteService, private router: Router) { }
+  newproduct:any=[];
 
   ngOnInit(): void {
+    this.newproduct=this.FavouriteService.getAllProduct();
+  }
+  deleteproduct(productId: any) {
+    this.newproduct.splice(productId-1,1);
+    console.log(this.newproduct)
   }
 
 }
