bf916b9bc1aa4af89816537c1b4a1d8e32001269
Ahmed Khalaf <ahmedkhalaf22696@gmail.com>
2022-11-16 22:51:20 +0200

    my-prof

--- src/app/body/profile/my-profile/my-profile.component.ts
+++ src/app/body/profile/my-profile/my-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fireStore:AngularFirestore) { }
+  person:any;
+  getdetails()
+  {
+    this.fireStore.collection("users").doc("15YrfZOFEWQW3AkdjKhUpuRBunM2").valueChanges()
+    .subscribe(val=>{
+      this.person=val;
+      console.log(this.person);
+      console.log(this.person.firstName);
 
+    });
+  }
   ngOnInit(): void {
+    this.getdetails();
   }
 
 }
